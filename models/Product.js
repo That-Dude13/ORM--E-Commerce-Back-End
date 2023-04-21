9656b821dd4edc19b343d0f5f42e4c62710bffc6
Change Agent <keasiley@icloud.com>
2023-04-21 16:44:46 -0500

    All databases in the Models folder have been updated.

--- models/Product.js
+++ models/Product.js
@@ -9,8 +9,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:true,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+      validate
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Category',
+        key: 'id',
+      },
+       // define columns
+    },
   },
+  
   {
     sequelize,
     timestamps: false,
