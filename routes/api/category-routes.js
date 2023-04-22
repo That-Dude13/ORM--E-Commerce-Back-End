868903d03a164d944972c2e41d261b0a58e3118d
Change Agent <keasiley@icloud.com>
2023-04-22 16:16:36 -0500

    category-routes.js updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,24 +4,73 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
+  try {
+    const productCategories = Category.findAll();
+    res.status(200).json(productCategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
 router.get('/:id', (req, res) => {
+  try {
+    const productCategories = productCategories.findByPk(req.params.id, {
+      
+      include: [{ model: Category, through: Product, as: 'category_id' }]
+    });
+
+    if (!locationData) {
+      res.status(404).json({ message: 'No product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(locationData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+    try {
+      const productCategories = Category.create(req.body);
+      res.status(200).json(productCategories);
+    } catch (err) {
+      res.status(400).json(err);
+    }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:product_id', (req, res) => {
+   const product_id = Category.update(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+    },
+    {
+      where: {
+        product_id: req.params.product_id
+      },
+    }
+  );
+
+  return res.json(product_id);
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:product_id', (req, res) => {
+  const product_id = product_id.destroy({
+    where: {
+      product_id: req.params.product_id,
+    },
+  });
+
+  return res.json(product_id);
   // delete a category by its `id` value
 });
 
